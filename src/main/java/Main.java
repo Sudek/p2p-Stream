bcf0485a14ba28cf361b050f62fafe48e1f57502
Andrey Sviridov <andrey.sviridov4@gmail.com>
2016-02-06 17:38:05 +0300

    Added treeTrain and sort method.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,11 @@ import org.slf4j.*;
 
 public class Main {
 
-  public static void main(String [] args)
-  {
+  public static void main(String [] args) {
     Logger logger = LoggerFactory.getLogger(Main.class);
     logger.info("Hi, I'm logger! Nice to meet you!");
     Tree tree = new Tree();
-    tree.treeInitialization(10);
+//    tree.treeInitialization(10);
+    tree.treeTrain(10);
   }
 }
