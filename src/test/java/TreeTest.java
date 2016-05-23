eb24734d29ab588ff4a341808cc95491b36ca8a3
Andrey Sviridov <andrey.sviridov4@gmail.com>
2016-05-23 21:23:51 +0300

    Deleted map. Added implementation for starTree.

--- src/test/java/TreeTest.java
+++ src/test/java/TreeTest.java
@@ -2,22 +2,20 @@ import org.junit.Test;
 
 public class TreeTest {
 
+  Tree tree = new Tree();
+
   @Test
   public void shouldInit() {
-    Tree tree = new Tree();
     tree.treeInitialization(10);
-    tree.treeTrain(10);
-  }
+}
 
   @Test
   public void shouldTrain() {
-    Tree tree = new Tree();
     tree.treeTrain(10);
   }
 
   @Test
   public void shouldStar() {
-    Tree tree = new Tree();
-    tree.starTree(10);
+    tree.starTree(6);
   }
 }
